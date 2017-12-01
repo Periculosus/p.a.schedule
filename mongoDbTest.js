1bc0f9e8886b3ba7eb27986efda83ae99ae75a12
alex-babaevsky <pariculosus@gmail.com>
2017-12-01 15:21:06 +0200

    added electron, added electron-reload, added mongodb - edited tests

--- mongoDbTest.js
+++ mongoDbTest.js
@@ -1,36 +1,32 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/testongo";
-window.inputVar = 0;
 
 $("#testInput").keyup(function () {
-    inputVar = $(this).val();
-    // console.log("input is " + inputVar);
+    window.myobj = {
+        _id: parseInt((Math.random() * 1000000).toFixed(0)),
+        name: $(this).val(),
+        array1: [
+            "Programming",
+            "Administrating",
+            "Abracadabring"
+        ],
+        objects1: [
+            {
+                obj1: "info 1",
+                obj2: "info 2",
+                obj3: "info 3"
+            }
+        ]
+    };
 });
-// var inputVar = "sjst-dfgdhf";
-
-var myobj = {
-    _id: inputVar,
-    name: "Иосиф Борода",
-    array1: [
-        "Programming",
-        "Administrating",
-        "Abracadabring"
-    ],
-    objects1: [{
-        obj1: "info 1",
-        obj2: "info 2",
-        obj3: "info 3"
-    }]
-};
 
 $("#writeDocIntoDB").click(function () {
-    // window.id = $("#testInput").val();
 
     MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
+        if (err) throw alert(err);
 
         db.collection("teachers").insertOne(myobj, function(err, res) {
-            if (err) throw err;
+            if (err) throw alert(err);
             // console.log("Operation complete");
         });
 
