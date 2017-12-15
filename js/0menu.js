7c18a2e797d7b9d851a3ccca40cba32b6f91165d
alex-babaevsky <pariculosus@gmail.com>
2017-12-15 16:47:26 +0200

    menu added, groups added

--- js/0menu.js
+++ js/0menu.js
@@ -3,7 +3,7 @@ const path = require('path');
 const url = require('url');
 const BrowserWindow = remote.BrowserWindow;
 
-$("#test").click(function () {
+$("#groupsWindow").click(function () {
     // var car = $("#selectMenu").val().toString();
     // if(car == "saab"){
     //     alert("its works - car is " + car);
@@ -14,7 +14,7 @@ $("#test").click(function () {
 
     var win = new BrowserWindow({ width: 800, height: 600 });
     win.loadURL(url.format({
-        pathname: path.join(__dirname, '1students.html')
+        pathname: path.join(__dirname, '4groups.html')
     }));
 
 });
